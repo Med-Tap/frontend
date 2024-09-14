423c7ca8485ae25f1ccd50acafe93ced5732acfa
Eric Chen <echen9870@gmail.com>
2024-09-14 04:54:38 -0400

    Added NFC search to each sidebar

--- src/app/allergy/page.js
+++ src/app/allergy/page.js
@@ -147,6 +147,21 @@ export default function dashboard(props) {
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
                 <ul role="list" className="-mx-2 space-y-1">
+                  <div className="flex items-center space-x-2">
+                    <input
+                      id="hashID"
+                      name="hashID"
+                      type="text"
+                      placeholder="Enter the User NFC Key"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    <button
+                      type="submit"
+                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Search
+                    </button>
+                  </div>
                   {navigation.map((item) => (
                     <li key={item.name}>
                       <a
