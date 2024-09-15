92a5f162a79616398cd710b4f9d273c2b4cc43bf
Ashitha Gowda <ashithapgowdaapg@gmail.com>
2024-09-14 21:58:46 -0400

    check if the user exists and generate the hash only first time

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -6,29 +6,36 @@ import { NextResponse } from 'next/server';
 export const GET = handleAuth({
   login: handleLogin({
     returnTo: '/dashboard',
+    authorizationParams: {
+      scope: 'openid profile email',  // Request ID token, profile, and email
+    },
   }),
   callback: handleCallback({
-    afterCallback: async (req, res, session) => {
+    afterCallback: async (req,res) => {
       const decoded = jwt.decode(res.idToken);
-
       const { email, given_name, family_name } = decoded;
       try {
         // Make the API call to your server
-        const apiResponse = await fetch('http://localhost:8080/user/create', {
+        const userEsists = await fetch(`${process.env.SERVER_URL}/user/get/${email}`, {
+          method: 'GET',
+        }); 
+        if(userEsists.isExisting == undefined || userEsists.isExisting == 'undefined'){
+          const apiResponse = await fetch(`${process.env.SERVER_URL}/user/create`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({ email, given_name, family_name }),  // Customize based on API requirements
         });
-
         // If the API call is unsuccessful, throw an error
         if (!apiResponse.ok) {
-          throw new Error('Failed to save user details',apiResponse);
+          return NextResponse.redirect(new URL('/api/auth/login', process.env.AUTH0_BASE_URL));
+        }else{
+          return NextResponse.redirect(new URL('/dashboard', process.env.AUTH0_BASE_URL));
         }
-
-        // On success, redirect the user to /dashboard
-        // return NextResponse.redirect(new URL('/dashboard', process.env.AUTH0_BASE_URL));
+        } else{
+          return NextResponse.redirect(new URL('/api/auth/login', process.env.AUTH0_BASE_URL));
+        }
       } catch (error) {
         console.error('API call failed:', error);
 
